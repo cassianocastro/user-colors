a643ecb8f848676dcb5c9d82e9024d9447921b42
Cassiano Castro <development.castro@gmail.com>
2024-03-25 15:43:58 -0300

    Restructured HTML content

--- app/View/UsersFormView.php
+++ app/View/UsersFormView.php
@@ -31,31 +31,34 @@ final class UsersFormView extends View
         return <<<HTML
             <form action="{$this->qs->create("users", "insert")}" method="POST" spellcheck="true" autocomplete="off">
                 <div>
-                    <div>
-                        <fieldset>
-                            <legend>New User</legend>
-                            <div>
-                                <label for="">User Name:</label>
-                                <input type="text" name="name" autofocus maxlength="100" pattern="(\w)+" title="Somente caracteres de A-Z e/ou a-z, 0-9 e _" placeholder="user_Name123" required>
+                    <fieldset>
+                        <legend>New User</legend>
 
-                                <label for="">User e-mail:</label>
-                                <input type="email" name="email" maxlength="100" placeholder="example@example.com" required>
+                        <label title="Somente caracteres de A-Z e/ou a-z, 0-9 e _">
+                            User Name:
 
-                                <label for="">Color:</label>
-                                <select id="" name="colors[]" multiple title="Você pode selecionar mais de uma opção segurando Ctrl e clicando com o mouse." required>
-                                    $this->options
-                                </select>
-                            </div>
-                        </fieldset>
-                    </div>
-                    <div>
-                        <menu>
-                            <div>
-                                <li><button type="reset">Limpar</button></li>
-                                <li><button type="submit">Enviar</button></li>
-                            </div>
-                        </menu>
-                    </div>
+                            <input type="text" name="name" maxlength="100" placeholder="user_Name123" pattern="(\w)+" autofocus required>
+                        </label>
+
+                        <label>
+                            User e-mail:
+
+                            <input type="email" name="email" maxlength="100" placeholder="example@example.com" required>
+                        </label>
+
+                        <label title="Você pode selecionar mais de uma opção segurando Ctrl e clicando com o mouse.">
+                            Color:
+
+                            <select name="colors[]" multiple required>
+                                $this->options
+                            </select>
+                        </label>
+                    </fieldset>
+
+                    <menu>
+                        <li><button type="reset">Limpar</button></li>
+                        <li><button type="submit">Enviar</button></li>
+                    </menu>
                 </div>
             </form>
         HTML;
@@ -66,33 +69,36 @@ final class UsersFormView extends View
         return <<<HTML
             <form action="{$this->qs->create("users", "update")}" method="POST" spellcheck="true" autocomplete="off">
                 <div>
-                    <div>
-                        <fieldset>
-                            <legend>Edit User</legend>
-                            <div>
-                                <input type="hidden" name="id" value="{$this->user->getID()}">
+                    <fieldset>
+                        <legend>Edit User</legend>
 
-                                <label>User Name:</label>
-                                <input type="text" name="name" value="{$this->user->getName()}" autofocus maxlength="100" pattern="(\w)+" title="Somente caracteres de A-Z e/ou a-z, 0-9 e _" placeholder="user_Name123" required>
+                        <input type="hidden" name="id" value="{$this->user->getID()}">
 
-                                <label>User e-mail:</label>
-                                <input type="email" name="email" value="{$this->user->getEmail()}" maxlength="100" placeholder="example@example.com" required>
+                        <label title="Somente caracteres de A-Z e/ou a-z, 0-9 e _">
+                            User Name:
 
-                                <label>Color:</label>
-                                <select id="" name="colors[]" multiple title="Você pode selecionar mais de uma opção segurando Ctrl e clicando com o mouse." required>
-                                    $this->options
-                                </select>
-                            </div>
-                        </fieldset>
-                    </div>
-                    <div>
-                        <menu>
-                            <div>
-                                <li><button type="reset">Limpar</button></li>
-                                <li><button type="submit">Enviar</button></li>
-                            </div>
-                        </menu>
-                    </div>
+                            <input type="text" name="name" value="{$this->user->getName()}" maxlength="100" placeholder="user_Name123" pattern="(\w)+" autofocus required>
+                        </label>
+
+                        <label>
+                            User e-mail:
+
+                            <input type="email" name="email" value="{$this->user->getEmail()}" maxlength="100" placeholder="example@example.com" required>
+                        </label>
+
+                        <label title="Você pode selecionar mais de uma opção segurando Ctrl e clicando com o mouse.">
+                            Color:
+
+                            <select name="colors[]" multiple required>
+                                $this->options
+                            </select>
+                        </label>
+                    </fieldset>
+
+                    <menu>
+                        <li><button type="reset">Limpar</button></li>
+                        <li><button type="submit">Enviar</button></li>
+                    </menu>
                 </div>
             </form>
         HTML;
