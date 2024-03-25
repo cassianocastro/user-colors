158acee0f03d7a8d5e609cafa9fe22488f878860
Cassiano Castro <development.castro@gmail.com>
2024-03-25 14:18:02 -0300

    Updated URL of the linked resources

--- public/templates/cv.php
+++ public/templates/cv.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-BR" dir="ltr">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="preload" as="script" href="https://cdn.jsdelivr.net/npm/less@4">
-  <link rel="preload" as="object" type="application/pdf" href="src/lib/Curriculum.pdf">
+  <link rel="preload" as="object" type="application/pdf" href="lib/Curriculum.pdf">
 
-  <link rel="stylesheet/less" type="text/css" href="src/public/less/cv.less">
+  <link rel="stylesheet/less" type="text/css" href="public/styles/less/cv.less">
 
   <title>Test | CV</title>
 </head>
@@ -27,7 +27,7 @@
             </div>
           </header>
 
-          <object data="src/lib/Curriculum.pdf" type="application/pdf"></object>
+          <object data="lib/Curriculum.pdf" type="application/pdf"></object>
         </div>
       </section>
     </div>
