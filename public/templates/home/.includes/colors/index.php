798053f9b956b942092b5a4c0b08630f2b7b60da
Cassiano Castro <development.castro@gmail.com>
2024-04-04 14:49:57 -0300

    Removed HTML head & body elements

--- public/templates/home/.includes/colors/index.php
+++ public/templates/home/.includes/colors/index.php
@@ -1,80 +1,50 @@
-<?php $colors = [] ?>
+<section>
+  <div>
 
-<!DOCTYPE html>
-<html lang="pt-BR" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <header>
+      <div>
+        <h1>All Colors registered.</h1>
+      </div>
+    </header>
 
-  <link rel="preload" as="script" href="https://cdn.jsdelivr.net/npm/less@4">
-
-  <link rel="stylesheet/less" type="text/css" href="public/templates/colors/index.less">
-
-  <title>Test | Colors</title>
-</head>
-<body>
-  <?php require_once __DIR__ . '/../.includes/noscript/index.inc'; ?>
-
-  <?php require_once __DIR__ . '/../.includes/header/index.inc'; ?>
-
-  <main>
-    <div>
-      <section>
-        <div>
-
-          <header>
-            <div>
-              <h1>All Colors registered.</h1>
-            </div>
-          </header>
-
-          <table>
-            <caption>
-              Colors Table
-            </caption>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Código Hexadecimal</th>
-                <th>Ação</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php foreach ($colors as $color) : ?>
-                <tr>
-                  <td><?= $color->getID() ?></td>
-                  <td><?= $color->getName() ?></td>
-                  <td><?= $color->getHexCode() ?></td>
-                  <td>
-                    <a href="<?= $this->qs->create("colors", "showForm", $color->getID()) ?>" class="material-icons md-18">
-                      edit
-                    </a>
-                    <a href="<?= $this->qs->create("colors", "delete", $color->getID()) ?>" class="material-icons md-18">
-                      delete
-                    </a>
-                  </td>
-                </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
-
-          <footer>
-            <div>
-              <a href="<?= $this->qs->create("colors", "showForm") ?>">
-                Add a new Color
+    <table>
+      <caption>
+        Colors Table
+      </caption>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Código Hexadecimal</th>
+          <th>Ação</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($colors as $color) : ?>
+          <tr>
+            <td><?= $color->getID() ?></td>
+            <td><?= $color->getName() ?></td>
+            <td><?= $color->getHexCode() ?></td>
+            <td>
+              <a href="<?= $this->qs->create("colors", "showForm", $color->getID()) ?>" class="material-icons md-18">
+                edit
               </a>
-            </div>
-          </footer>
+              <a href="<?= $this->qs->create("colors", "delete", $color->getID()) ?>" class="material-icons md-18">
+                delete
+              </a>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
 
-        </div>
-      </section>
-    </div>
-  </main>
+    <footer>
+      <div>
+        <a href="<?= $this->qs->create("colors", "showForm") ?>">
+          Add a new Color
+        </a>
+      </div>
+    </footer>
 
-  <?php require_once __DIR__ . '/../.includes/footer/index.inc'; ?>
-
-  <script defer type="module" src="https://cdn.jsdelivr.net/npm/less@4"></script>
-</body>
-</html>
+  </div>
+</section>
--- public/templates/home/.includes/users/index.php
+++ public/templates/home/.includes/users/index.php
@@ -1,79 +1,49 @@
-<?php $users = [] ?>
+<section>
+  <div>
+    <header>
+      <div>
+        <h1>All users registered.</h1>
+      </div>
+    </header>
 
-<!DOCTYPE html>
-<html lang="pt-BR" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <link rel="preload" as="script" href="https://cdn.jsdelivr.net/npm/less@4">
-
-  <link rel="stylesheet/less" type="text/css" href="public/templates/users/index.less">
-
-  <title>Test | Users</title>
-</head>
-<body>
-  <?php require_once __DIR__ . '/../.includes/noscript/index.inc'; ?>
-
-  <?php require_once __DIR__ . '/../.includes/header/index.inc'; ?>
-
-  <main>
-    <div>
-      <section>
-        <div>
-          <header>
-            <div>
-              <h1>All users registered.</h1>
-            </div>
-          </header>
-
-          <table>
-            <caption>
-              Users Table
-            </caption>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Ação</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php foreach ($users as $user): ?>
-                <tr>
-                  <td><?= $user->getID() ?></td>
-                  <td><?= $user->getName() ?></td>
-                  <td><?= $user->getEmail() ?></td>
-                  <td>
-                    <a href="<?= $this->qs->create("users", "showForm", $user->getID()) ?>" class="material-icons md-18">
-                      edit
-                    </a>
-                    <a href="<?= $this->qs->create("users", "delete", $user->getID()) ?>" class="material-icons md-18">
-                      delete
-                    </a>
-                  </td>
-                </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
-
-          <footer>
-            <div>
-              <a href="<?= $this->qs->create("users", "showForm") ?>">
-                Add a new User
+    <table>
+      <caption>
+        Users Table
+      </caption>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Email</th>
+          <th>Ação</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($users as $user): ?>
+          <tr>
+            <td><?= $user->getID() ?></td>
+            <td><?= $user->getName() ?></td>
+            <td><?= $user->getEmail() ?></td>
+            <td>
+              <a href="<?= $this->qs->create("users", "showForm", $user->getID()) ?>" class="material-icons md-18">
+                edit
               </a>
-            </div>
-          </footer>
+              <a href="<?= $this->qs->create("users", "delete", $user->getID()) ?>" class="material-icons md-18">
+                delete
+              </a>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
 
-        </div>
-      </section>
-    </div>
-  </main>
+    <footer>
+      <div>
+        <a href="<?= $this->qs->create("users", "showForm") ?>">
+          Add a new User
+        </a>
+      </div>
+    </footer>
 
-  <?php require_once __DIR__ . '/../.includes/footer/index.inc'; ?>
-
-  <script defer type="module" src="https://cdn.jsdelivr.net/npm/less@4"></script>
-</body>
-</html>
+  </div>
+</section>
