ae6e74f1edd0e0db7ac9379bd9e955224137a9f1
Cassiano Castro <development.castro@gmail.com>
2024-04-05 15:56:10 -0300

    Added content to generate component

---
+++ public/templates/home/.includes/context/index.php
@@ -0,0 +1,13 @@
+<menu id="context">
+  <li>
+    <button type="button" title="Update" class="material-icons md-24">
+      edit
+    </button>
+  </li>
+
+  <li>
+    <button type="button" title="Remove" class="material-icons md-24">
+      delete
+    </button>
+  </li>
+</menu>
