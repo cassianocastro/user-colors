12e705547bee98c71c18577fab4a33ae97823b7c
Cassiano Castro <development.castro@gmail.com>
2024-04-05 14:19:30 -0300

    File addition

---
+++ public/templates/home/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import { index as header } from "../.includes/header/index.js";
+import { index as users } from "./.includes/users/index.js";
+
+/**
+ *
+ */
+function index()
+{
+    header();
+    users();
+}
+
+index();
