2287a31d1a58827fe961e63046605a9e957b37a3
Cassiano Castro <development.castro@gmail.com>
2024-04-05 14:54:38 -0300

    Fix: updated string argument on ::querySelector() method

--- public/templates/home/.includes/users/index.js
+++ public/templates/home/.includes/users/index.js
@@ -5,7 +5,7 @@
  */
 function index()
 {
-    const dialog = document.querySelector("dialog");
+    const dialog = document.querySelector("dialog:first-of-type");
     const button = document.querySelector("button[title~='user']");
 
     button.addEventListener("click", () => dialog.show());
