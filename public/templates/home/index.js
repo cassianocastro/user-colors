04747d0f92c5c4bc51b7fa145401a710bc161fff
Cassiano Castro <development.castro@gmail.com>
2024-04-05 14:36:11 -0300

    Import module

--- public/templates/home/index.js
+++ public/templates/home/index.js
@@ -2,6 +2,7 @@
 
 import { index as header } from "../.includes/header/index.js";
 import { index as users } from "./.includes/users/index.js";
+import { index as udialog } from "./.includes/users-dialog/index.js";
 
 /**
  *
@@ -10,6 +11,7 @@ function index()
 {
     header();
     users();
+    udialog();
 }
 
 index();
