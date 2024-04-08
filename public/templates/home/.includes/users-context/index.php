e2bc5273727234a70025708679e60704b1884d3d
Cassiano Castro <development.castro@gmail.com>
2024-04-07 23:15:15 -0300

    Added content

---
+++ public/templates/home/.includes/users-context/index.php
@@ -0,0 +1,19 @@
+<menu id="users-context">
+  <li>
+    <button type="button" title="Update" class="material-icons md-24">
+      edit
+    </button>
+  </li>
+
+  <li>
+    <button type="button" title="Details" class="material-icons md-24">
+      more_horiz
+    </button>
+  </li>
+
+  <li>
+    <button type="button" title="Remove" class="material-icons md-24">
+      delete
+    </button>
+  </li>
+</menu>
