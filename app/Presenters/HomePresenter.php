fcfd354a49be8cc9804222bdd5c102bf24ccabca
Cassiano Castro <development.castro@gmail.com>
2024-04-15 14:45:36 -0300

    Added code to recovery dataset of db

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Model\Entities\{ Color, User };
+use App\Model\Tables\{ ColorsTable, UsersTable };
 use App\Model\Utils\{ HtmlDocument, Response };
 
 /**
@@ -13,13 +15,46 @@ final class HomePresenter
 
     public function index(): Response
     {
-        [ $users, $colors ] = [ [], [] ];
+        $r_users  = (new UsersTable())->getAll();
+        $r_colors = (new ColorsTable())->getAll();
+
+        // echo "<pre>", var_dump($r_users, $r_colors), "</pre>";
+
+        $users = [];
+
+        foreach ($r_users as $user)
+        {
+            extract($user);
+
+            $users[$id] = new User($name, $email);
+        }
+
+        // array_shift($users);
+
+        $colors = [];
+
+        foreach ($r_colors as $color)
+        {
+            extract($color);
+
+            $colors[$id] = new Color($name, $hexcode);
+        }
+
+        // array_shift($colors);
+
+        // echo "<pre>", var_dump($users, $colors), "</pre>";
 
         return new Response(
             [
                 "Content-Type: text/html"
             ],
-            new HtmlDocument("home/index.php")
+            new HtmlDocument(
+                "home/index.php",
+                [
+                    "users"  => $users,
+                    "colors" => $colors
+                ]
+            )
         );
     }
 }
