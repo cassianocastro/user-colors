fe964040c177a80620880c535a8222a996ccfc23
Cassiano Castro <development.castro@gmail.com>
2024-04-15 19:02:23 -0300

    Added verification to document's type

--- app/Model/Utils/Browser.php
+++ app/Model/Utils/Browser.php
@@ -3,14 +3,29 @@ declare(strict_types=1);
 
 namespace App\Model\Utils;
 
+use Twig\{ Environment, Loader\FilesystemLoader };
+
 /**
  *
  */
 final class Browser
 {
 
+    private const PATH = __DIR__ . "/../../../public/templates/";
+
     static public function render(Response $response): void
     {
-        print $response->getBody()->getContent();
+        $body = $response->getBody();
+
+        if ( $body instanceof HtmlDocument )
+        {
+            $loader = new FilesystemLoader([self::PATH, self::PATH . ".includes"]);
+            $env    = new Environment($loader);
+
+            $name   = $body->getName();
+            $params = $body->getParams();
+
+            print $env->render($name, $params);
+        }
     }
 }
