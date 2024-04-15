7c69472d11d4aecc339e0c5dd483c71cdbc11c43
Cassiano Castro <development.castro@gmail.com>
2024-04-15 14:40:03 -0300

    Added code to generate dialog form with values

--- public/templates/home/.includes/colors-context/index.js
+++ public/templates/home/.includes/colors-context/index.js
@@ -5,7 +5,21 @@
  */
 function index()
 {
-    //
+    const menu   = document.querySelector("#colors-context");
+
+    {
+        const dialog = document.querySelector("#cd");
+
+        const name = dialog.querySelector("input[name='name']");
+        const code = dialog.querySelector("input[name='hexcode']");
+
+        name.value = "Pink";
+        code.value = "#C35DB2";
+
+        const button = menu.querySelector("[title='Update']");
+
+        button.addEventListener("click", () => dialog.show());
+    }
 }
 
 export { index };
